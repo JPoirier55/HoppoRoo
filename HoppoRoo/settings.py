4814345ffed822ba295071ee9f85558686d93a7d
Jake <jake.poirier55@gmail.com>
2016-02-20 22:25:04 -0700

    Migrate and set db and static

--- HoppoRoo/settings.py
+++ HoppoRoo/settings.py
@@ -120,5 +120,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
-
-STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+print STATIC_URL
+print BASE_DIR
+STATIC_ROOT = os.path.join(BASE_DIR, "HLS/static/")
