0611a0b138636deaa3fbae1efc1c95a223ce83f1
Jake <jake.poirier55@gmail.com>
2016-02-27 13:51:06 -0700

    double mint jum

--- static/js/hls.js
+++ static/js/hls.js
@@ -4,22 +4,38 @@
 
 function updateFunction(){
     var interval = setInterval(update, 1000);
-        function update() {
-            $.ajax({
-                type: "GET",
-                url: "http://192.168.42.15?callback=jsonCallBack",
-                async: false,
-                jsonpCallback: 'jsonCallBack',
-                contentType: "application/json",
-                dataType: 'jsonp',
-                success: function (json) {
-                    door1 = json['door1'];
-                    door2 = json['door2'];
-                    console.log(door1, door2);
-                },
-                error: function(){
-                    console.log('shit');
-                }
+    function update() {
+        $.ajax({
+            type: "GET",
+            url: "http://192.168.42.15?callback=jsonCallBack",
+            async: false,
+            jsonpCallback: 'jsonCallBack',
+            contentType: "application/json",
+            dataType: 'jsonp',
+            success: function (json) {
+                door1 = json['door1'];
+                door2 = json['door2'];
+                console.log(door1, door2);
+            },
+            error: function(){
+                console.log('shit');
+            }
+        });
+         $.ajax({
+            type: "GET",
+            url: "http://192.168.42.16?callback=jsonCallBack",
+            async: false,
+            jsonpCallback: 'jsonCallBack',
+            contentType: "application/json",
+            dataType: 'jsonp',
+            success: function (json) {
+                door1 = json['door1'];
+                door2 = json['door2'];
+                console.log(door1, door2);
+            },
+            error: function(){
+                console.log('shit');
+            }
         });
     }
 }
