96165664266e645d9b930151e75f4ee56276b472
Jake <jake.poirier55@gmail.com>
2016-02-27 21:25:31 -0700

    update to js method2

--- static/js/hls.js
+++ static/js/hls.js
@@ -2,38 +2,30 @@
  * Created by Jake on 2/27/2016.
  */
 
-//function updateFunction(){
+var interval = setInterval(queue, 1000);
+console.log(interval);
+
+function queue(){
+    update("192.168.42.15");
+}
+function update(ip) {
+    $.ajax({
+        type: "GET",
+        url: "http://" + ip +"?callback=jsonCallBack",
+        async: false,
+        jsonpCallback: 'jsonCallBack',
+        contentType: "application/json",
+        dataType: 'jsonp',
+        success: function (json) {
+            console.log(json);
+        },
+        error: function(){
+            console.log('shit');
+        }
+    });
+}
 
 
 
-    //var interval = setInterval(update, 1000);
-    //function update() {
-    //    $.ajax({
-    //        type: "GET",
-    //        url: "http://192.168.42.15?callback=jsonCallBack",
-    //        async: false,
-    //        jsonpCallback: 'jsonCallBack',
-    //        contentType: "application/json",
-    //        dataType: 'jsonp',
-    //        success: function (json) {
-    //            door1 = json['door1'];
-    //            door2 = json['door2'];
-    //            console.log(door1, door2);
-    //        },
-    //        error: function(){
-    //            console.log('shit');
-    //        }
-    //    });
-    //
-    //}
-
-
-function getData(url, id1, id2) {
-        $.get(url, Common.genSafeId(), function(data) {
-            $(id1).html(data);
-            $(id2).addClass("empty");
-            console.log(data);
-        });
-    }
 
 
