429b8d38eed1d91f05f9c229c27f9bd06786c003
Jake <jake.poirier55@gmail.com>
2016-08-07 09:21:23 -0600

    Student db calls and page

--- HLS/models.py
+++ HLS/models.py
@@ -4,6 +4,7 @@ from django.db import models
 
 
 class Quiz(models.Model):
+    id = models.IntegerField(primary_key=True)
     quizjson = models.TextField(null=True)
     name = models.CharField(max_length=256, default='')
 
@@ -21,16 +22,19 @@ class Student(models.Model):
 
 class Device(models.Model):
     id = models.IntegerField(primary_key=True, default=None)
-    student = models.OneToOneField(Student, on_delete=models.CASCADE)
+    student = models.OneToOneField(Student)
 
     def __str__(self):
         return "Device: {0} - Linked to {1}".format(self.id, self.student.name)
 
 
 class Results(models.Model):
-    quiz = models.OneToOneField(Quiz, on_delete=models.CASCADE)
-    student = models.OneToOneField(Student)
+    quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
+    student = models.ForeignKey(Student)
     score = models.IntegerField(default=0)
+
+    class Meta:
+        unique_together = ('quiz', 'student')
 
     def __str__(self):
         return "Student: {0} - Quiz: {1} - Score: {2}".format(self.student.name, self.quiz.name, self.score)
