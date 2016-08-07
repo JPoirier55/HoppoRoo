cd54823026db512f0d82a5d96077d71aa42565a2
Jake <jake.poirier55@gmail.com>
2016-08-07 11:24:31 -0600

    Results metrics page with db calls

---
+++ HLS/results.py
@@ -0,0 +1,27 @@
+from models import Quiz, Results
+import utils
+import json
+
+
+def results_metrics():
+    quizzes = Quiz.objects.all()
+    metrics = {}
+    for quiz in quizzes:
+        results = Results.objects.filter(quiz=quiz)
+        quiz_json = json.loads(quiz.quizjson)
+        scores = []
+        single_metrics = {}
+        for result in results:
+            scores.append(result.score)
+        single_metrics['scores'] = scores
+        single_metrics['name'] = quiz.name
+        single_metrics['num_of_questions'] = len(quiz_json['questions'])
+        mean = utils.average(scores)
+        single_metrics['class_av'] = mean
+        single_metrics['std_dev'] = utils.std_deviation(scores, mean)
+        single_metrics['subject'] = quiz.subject
+        single_metrics['high'] = max(scores)
+        single_metrics['low'] = min(scores)
+        single_metrics['class_median'] = utils.median(scores)
+        metrics[quiz.name] = single_metrics
+    return metrics
