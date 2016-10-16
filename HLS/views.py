078049448f1bbb080af5a934adb2cc2dbd5f38f0
Jake <jake.poirier55@gmail.com>
2016-10-16 08:51:36 -0600

    Switch to linux file dirs

--- HLS/views.py
+++ HLS/views.py
@@ -139,7 +139,7 @@ def help(request):
 
 def pdf_view(request):
     pdfs = []
-    dir = 'C:\\Users\\Jake\\git3\\Hopporoo\\static\\res\\'
+    dir = '/home/ubuntu/Hopporoo/Hopporoo/static/res/'
     for pdf in os.listdir(dir):
         if 'pdf' in pdf:
             pdf = {'name': pdf,
@@ -192,7 +192,7 @@ def upload_file(request):
     if request.method != 'POST':
         return HttpResponseBadRequest('Only POST requests are allowed')
     file = request.FILES['myfile']
-    with open('C:\\Users\\Jake\\git3\\Hopporoo\\static\\res\\%s' % file.name, 'wb+') as dest:
+    with open('/home/ubuntu/Hopporoo/Hopporoo/static/res/%s' % file.name, 'wb+') as dest:
         for chunk in file.chunks():
             dest.write(chunk)
     return HttpResponse("file uploaded")
